c2498f095ff8e464bb2b801e113d435023baa52b
Igor Alex de Castro <igoralex8701@gmail.com>
2025-08-12 22:57:27 -0300

    first commit

---
+++ src/main/java/com/dionariao/DionariaoApplication.java
@@ -0,0 +1,13 @@
+package com.dionariao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DionariaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DionariaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dionariao/DionariaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dionariao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DionariaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

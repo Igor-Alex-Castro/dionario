96c8f3446b2b33bc1f8836002317f9fcad3480e7
Igor Alex de Castro <igoralex8701@gmail.com>
2025-09-11 16:21:26 -0300

    Adicionado tratamento de erro para 409, 401, 403, 500

--- src/main/java/com/dionariao/handler/GlobalExceptionHandler.java
+++ src/main/java/com/dionariao/handler/GlobalExceptionHandler.java
@@ -1,11 +1,14 @@
 package com.dionariao.handler;
 
+import java.nio.file.AccessDeniedException;
 import java.time.LocalDateTime;
 import java.util.Map;
 import java.util.stream.Collectors;
 
+import javax.naming.AuthenticationException;
+
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.MissingServletRequestParameterException;
@@ -77,5 +80,68 @@ public class GlobalExceptionHandler {
 					"path", request.getRequestURI()
 					));
 	 }
+	 
+	// 409 Conflict - Violação de integridade no banco
+	 @ExceptionHandler(DataIntegrityViolationException.class)
+	 public ResponseEntity<Map<String, Object>>  handleDataIntegrityViolation(DataIntegrityViolationException ex,  HttpServletRequest request){
+		 return ResponseEntity.status(HttpStatus.CONFLICT).body(Map.of(
+				 
+				 "timestamp", LocalDateTime.now(),
+					"status", HttpStatus.CONFLICT,
+					"error", "Violação de integridade",
+					"message", ex.getMessage(),
+					"path", request.getRequestURI()
+				 )
+				 );
+				 
+	 }
+	 
+	 @ExceptionHandler(AuthenticationException.class)
+	    public ResponseEntity<Map<String, Object>> handleAuthenticationException(AuthenticationException ex, HttpServletRequest request) {
+	        return 
+	        		ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(Map.of(
+	       				 
+	       				 "timestamp", LocalDateTime.now(),
+	       					"status", HttpStatus.UNAUTHORIZED,
+	       					"error", "Não autenticado",
+	       					"message", ex.getMessage(),
+	       					"path", request.getRequestURI()
+	       				 )
+	       	);
+	    }
+	 
+	 	@ExceptionHandler(AccessDeniedException.class)
+	    public ResponseEntity<Map<String, Object>> handleAccessDenied(AccessDeniedException ex,  HttpServletRequest request) {
+	        return ResponseEntity.status(HttpStatus.FORBIDDEN).body(Map.of(
+      				 
+      				 "timestamp", LocalDateTime.now(),
+      					"status", HttpStatus.FORBIDDEN,
+      					"error", "Acesso negado",
+      					"message", ex.getMessage(),
+      					"path", request.getRequestURI()
+      				 )
+      	);
+	    }
+	 
+	 	// 500 Internal Server Error - Qualquer erro inesperado
+	    @ExceptionHandler(Exception.class)
+	    public ResponseEntity<Map<String, Object>> handleGenericException(Exception ex, HttpServletRequest request) {
+	    	return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(Map.of(
+      				 
+      				 "timestamp", LocalDateTime.now(),
+      					"status", HttpStatus.INTERNAL_SERVER_ERROR,
+      					"error", "Erro interno",
+      					"message", ex.getMessage(),
+      					"path", request.getRequestURI()
+      				 )
+      	);
+	    }
+	    
+			/*Metodo para ser utilizado para construir o json da padronização
+		 * private ResponseEntity<Map<String, Object>> buildResponse(HttpStatus status,
+		 * String error, String message) { return
+		 * ResponseEntity.status(status).body(Map.of( "timestamp", LocalDateTime.now(),
+		 * "status", status.value(), "error", error, "message", message )); }
+		 */
 	
 }
